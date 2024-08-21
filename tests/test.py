289f5d197b9f7c119e418adb51e81817aefd5675
Liam Robinson <robin502@purdue.edu>
2024-08-21 16:01:22 -0400

    project path for test

--- tests/test.py
+++ tests/test.py
@@ -2,10 +2,12 @@ import mesdf
 import trimesh
 import numpy as np
 import pytest
-
+import os
 
 def test_sdf():
-    obj = trimesh.load_mesh("sample-obj/boxframe.obj")
+    path = os.environ['PROJECT_DIR']
+    print(path)
+    obj = trimesh.load_mesh(os.path.join(PROJECT_DIR, "sample-obj", "boxframe.obj"))
     sdf = mesdf.SDF(verts=obj.vertices, faces=obj.faces)
     p = np.array([1.0, 2.0, 3.0])
     assert pytest.approx(sdf(p)[0]) == -2.9094794
